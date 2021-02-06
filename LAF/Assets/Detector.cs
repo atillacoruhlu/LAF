b75371e17fa628d08ca8e6fb9d0c662b628457ba
atilla <atillacoruhlu@hotmail.com>
2021-02-06 21:27:26 +0300

    First Day

---
+++ LAF/Assets/Detector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    public GameObject TouchedObject;
+    private void OnTriggerEnter(Collider other) 
+    {
+        TouchedObject=other.transform.parent.gameObject; 
+
+    }
+    private void OnTriggerExit(Collider other) {
+        TouchedObject=null;
+    }
+}
---
+++ LAF/Assets/Tentacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : MonoBehaviour
+{
+    private GameObject Parent;
+    public GameObject TouchedObject;
+    private void Awake() {
+        Parent=gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.name==gameObject.name)
+        {
+          TouchedObject=other.transform.parent.gameObject; 
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        TouchedObject=null;
+    }
+}
