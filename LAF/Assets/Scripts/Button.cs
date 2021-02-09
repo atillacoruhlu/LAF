8bb44a3b31f9b6cfc6d26223162ea2180c1ce319
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2021-02-09 11:15:43 +0300

    Add Button, Fix Prefabs

---
+++ LAF/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject Kapi;
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name[0]=='H') //If Holdable or Player enters
+        {
+            Kapi.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.name[0]=='H') //If Holdable or Player exits
+        {
+            Kapi.SetActive(true);
+        }
+    }
+}
